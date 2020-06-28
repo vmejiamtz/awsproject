f96d8344c50771ca51309534c9f6b615abc257ea
vmejiamtz <67496351+vmejiamtz@users.noreply.github.com>
2020-06-27 23:58:23 -0500

    Create S3AwsProjectApplication.java

---
+++ src/main/java/com/aws/S3AwsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3AwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3AwsProjectApplication.class, args);
+	}
+
+}
